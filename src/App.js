328226a33b00b85a792b7fc3e4e80dab63128321
Temirlan-Br <barakov.temirlan@gmail.com>
2023-11-16 15:26:57 +0600

    Initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <button>Incr</button>
+
+      <button>Decr</button>
+    </div>
+  );
+}
+
+export default App;
